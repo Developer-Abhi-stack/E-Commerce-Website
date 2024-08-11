45adcab88f8e597c63c5aa0f54d3b666e147607a
Abhishekkumar2001 <abhikum8131@gmail.com>
2024-08-11 10:53:21 +0530

    Contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,26 +38,28 @@ const Contact = () => {
               />
             </div>
             <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="message"
-            >
-              Message
-            </label>
-            <textarea
-              type="text"
-              name="message"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              rows={4}
-              placeholder="Enter your message here ....."
-              required
-            />
-          </div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="message"
+              >
+                Message
+              </label>
+              <textarea
+                type="text"
+                name="message"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                rows={4}
+                placeholder="Enter your message here ....."
+                required
+              />
+            </div>
             <div className="flex items-center justify-between">
               <button
                 type="submit"
                 className="bg-[#ffc801] hover:bg-white text-white hover:text-[#ffc801] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              >Get Quote</button>
+              >
+                Get Quote
+              </button>
             </div>
           </form>
         </div>
